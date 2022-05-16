50092dc60b117e6d1d72c0aedf06a7683dfc952a
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-05-16 14:35:08 -0300

    Update cors para false

--- src/serverMultaBiblioteca.js
+++ src/serverMultaBiblioteca.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.all('*', function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Credentials', 'false');
 	res.header('Access-Control-Allow-Methods', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
